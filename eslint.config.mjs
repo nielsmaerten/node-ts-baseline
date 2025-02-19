32e821efe09dcd9d1eec74dee9b8586e7325bcdc
Niels Maerten <niels@maerten.be>
2025-02-19 11:56:12 +0100

    Baseline commit
    Typescript Node project with:
    - Typescript
    - Husky commit hooks
    - Lint-staged
    - ESLint checking
    - Stylistic formatting
    - nvmrc
    - yarn

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+    ],
+  },
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+  },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  stylistic.configs.customize({
+    semi: true,
+  }),
+];
